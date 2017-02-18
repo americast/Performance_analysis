0062a8910d44e0aac95ddedb35a3186f29120614
americast <sayan.sinha@iitkgp.ac.in>
2017-02-18 21:47:10 +0530

    Python and julia ready

---
+++ merge.jl
@@ -0,0 +1,46 @@
+function man(arr, beg, mid1, mid2, endn)
+  temp=[]
+  i=beg; j=mid2
+  while (true)
+    if (i>mid1 || j>endn)
+      break
+    end
+    if (arr[i]<=arr[j])
+      push!(temp,arr[i])
+      i+=1; continue
+    else
+      push!(temp,arr[j])
+      j+=1; continue
+    end
+  end
+  while (i<=mid1)
+    push!(temp,arr[i])
+    i+=1
+  end
+  while(j<=endn)
+    push!(temp,arr[j])
+    j+=1
+  end
+  j=1
+  for i in beg:endn
+    arr[i]=temp[j]
+    j+=1
+  end
+end
+
+
+function pungi(arr,l,r)
+  if (l<r)
+    pungi(arr,l,div((l+r),2))
+    pungi(arr,(1+div((l+r),2)),r)
+    man(arr,l,div((l+r),2),div((l+r),2)+1,r)
+  end
+end
+
+
+arr=map(x->parse(Int,x),split(readline(STDIN)))
+n=size(arr)[1]
+tic()
+pungi(arr,1,n)
+println(arr)
+toc()
---
+++ merge.py
@@ -0,0 +1,39 @@
+def man(arr, beg, mid1, mid2, endn):
+  temp=[]
+  i=beg; j=mid2
+  while 1:
+    if (i>mid1 or j>endn):
+      break
+    if (arr[i]<=arr[j]):
+      temp.append(arr[i])
+      i+=1; continue
+    else:
+      temp.append(arr[j])
+      j+=1; continue
+  while (i<=mid1):
+    temp.append(arr[i])
+    i+=1
+  while(j<=endn):
+    temp.append(arr[j])
+    j+=1
+  j=0
+  for i in xrange(beg,endn+1):
+    arr[i]=temp[j]
+    j+=1
+
+
+def pungi(arr,l,r):
+  if (l<r):
+    pungi(arr,l,((l+r)/2))
+    pungi(arr,(1+((l+r)/2)),r)
+    man(arr,l,((l+r)/2),((l+r)/2)+1,r)
+
+
+import time
+arr=raw_input().split(' ')
+n=len(arr)
+for i in xrange(0,len(arr)): arr[i]=int(arr[i])
+tic=time.clock()
+pungi(arr,0,n-1)
+print(arr)
+print(time.clock()-tic)
